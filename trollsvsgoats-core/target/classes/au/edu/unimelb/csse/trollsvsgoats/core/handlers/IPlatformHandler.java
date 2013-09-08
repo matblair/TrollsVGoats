5f5bef9d15446c9c120f55baa2337adb2af06524
Mathew Blair <mblair@student.unimelb.edu.au>
2013-09-08 13:02:01 +1000

    updating

--- trollsvsgoats-core/target/classes/au/edu/unimelb/csse/trollsvsgoats/core/handlers/IPlatformHandler.java
+++
@@ -1,9 +0,0 @@
-package au.edu.unimelb.csse.trollsvsgoats.core.handlers;
-
-/*
- * Interface to handle view size of the game to different platforms (Java, HTML, etc.)
- */
-public interface IPlatformHandler {
-
-	public void setSize(int width, int height);
-}
